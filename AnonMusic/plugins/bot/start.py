7ad8a814ef726650e80db846d28ef6019b0acba7
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-08-21 00:26:46 +0530

    Update start.py

--- AnonMusic/plugins/bot/start.py
+++ AnonMusic/plugins/bot/start.py
@@ -164,7 +164,7 @@ async def welcome(client, message: Message):
                 if (ch.title and re.search(r'[\u1000-\u109F]', ch.title)) or \
                         (ch.description and re.search(r'[\u1000-\u109F]', ch.description)):
                     await blacklist_chat(message.chat.id)
-                    await message.reply_text("🚫 This group is not allowed to play songs\n\n⚙️ Contact us : @VibeBoysSupport")
+                    await message.reply_text("🚫 This group is not allowed to play songs\n\n⚙️ Contact us : @Orgdisguiser")
                     await app.send_message(LOGGER_ID, f"🚫 This group has been blacklisted automatically due to Myanmar characters in the chat title, description or message \n Title:{ch.title} \n ID:{message.chat.id}\n\n⚙️ Contact Any Admin : @VibeBotsSupport")
                     return await app.leave_chat(message.chat.id)
 
